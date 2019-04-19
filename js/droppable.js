6f4717ec6328dc754487ae8e42583b37dedbe35b
Iván Alonso <ivan@neverbot.com>
2019-04-20 00:15:00 +0200

    jshint

--- js/droppable.js
+++ js/droppable.js
@@ -4,7 +4,7 @@ Pixelazr.Droppable = function()
 {
   this.dropboxElement = null;
   this.reader = null;
-}
+};
 
 Pixelazr.Droppable.prototype.initialize = function (element)
 {
@@ -16,60 +16,60 @@ Pixelazr.Droppable.prototype.initialize = function (element)
   addEvent('dragleave', this.dropboxElement, this.stopDefault);
   addEvent('drop', this.dropboxElement, this.onDrop);
 
-  this.reader = new FileReader(); 
+  this.reader = new FileReader();
 
-  this.reader.onerror = function(e)  
+  this.reader.onerror = function(e)
   {
-    var evt = e || window.event; 
-    pixelazr.logs.inform('Error code: ' + evt.target.error); 
-  }
+    var evt = e || window.event;
+    pixelazr.logs.inform('Error code: ' + evt.target.error);
+  };
 
-  // Create a closure to capture the file information. 
-  this.reader.onload = function(e)  
-  { 
+  // Create a closure to capture the file information.
+  this.reader.onload = function(e)
+  {
     pixelazr.logs.inform('Image loaded');
 
-    var evt = e || window.event; 
+    var evt = e || window.event;
     var image = new Image();
-    image.src = evt.target.result;  
+    image.src = evt.target.result;
 
     pixelazr.art.setImage(image);
-  } 
-} 
+  };
+};
 
-Pixelazr.Droppable.prototype.stopDefault = function(e)  
-{ 
-  var evt = e || window.event; 
-  evt.stopPropagation(); 
-  evt.preventDefault(); 
-} 
+Pixelazr.Droppable.prototype.stopDefault = function(e)
+{
+  var evt = e || window.event;
+  evt.stopPropagation();
+  evt.preventDefault();
+};
 
-Pixelazr.Droppable.prototype.onDrop = function(e)  
-{ 
+Pixelazr.Droppable.prototype.onDrop = function(e)
+{
   pixelazr.logs.log('Droppable.onDrop', 'Image onDrop');
 
   // Clear DOM log elements
   pixelazr.logs.clearMessages();
 
-  var evt = e || window.event; 
-  evt.stopPropagation(); 
-  evt.preventDefault(); 
+  var evt = e || window.event;
+  evt.stopPropagation();
+  evt.preventDefault();
 
-  var readFileSize = 0; 
-  var files = e.dataTransfer.files; 
+  var readFileSize = 0;
+  var files = e.dataTransfer.files;
 
-  var file = files[0]; 
-  readFileSize += file.fileSize; 
+  var file = files[0];
+  readFileSize += file.fileSize;
 
   // Only process image files
-  var imageType = /image.*/; 
+  var imageType = /image.*/;
 
-  if (!file.type.match(imageType))  
-  { 
-    pixelazr.logs.inform('Drop only image files, please'); 
-    return; 
-  } 
+  if (!file.type.match(imageType))
+  {
+    pixelazr.logs.inform('Drop only image files, please');
+    return;
+  }
 
   // Read in the image file as a data url
-  pixelazr.droppable.reader.readAsDataURL(file); 
-} 
+  pixelazr.droppable.reader.readAsDataURL(file);
+};
